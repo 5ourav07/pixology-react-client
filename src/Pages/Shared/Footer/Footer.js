e56880bf78e3f388c4a952b19262ffa0ec26537b
Sourav Saha <shanto.sourav07@gmail.com>
2022-11-08 13:03:43 +0600

    Footer Done

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedinIn, FaGoogle, FaRegCopyright } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer-container">
+                <h1 className="footer-title"><FaRegCopyright /><span>Sourav Saha</span></h1>
+                <div className="footer-link">
+                    <Link className='footer-icon-link' to="#">Contact Me</Link>
+                    <Link className='footer-icon-link' to="#">Terms of Service</Link>
+                    <Link className='footer-icon-link' to="#">Privacy Policy</Link>
+                    <Link className='footer-icon-link' to="#">Privacy Settings</Link>
+                </div>
+                <div className='footer-link'>
+                    <Link className='footer-icon-link'><FaFacebook /></Link>
+                    <Link className='footer-icon-link'><FaGithub /></Link>
+                    <Link className='footer-icon-link'><FaGoogle /></Link>
+                    <Link className='footer-icon-link'><FaInstagram /></Link>
+                    <Link className='footer-icon-link'><FaLinkedinIn /></Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
