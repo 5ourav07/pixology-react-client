ab24779e1073c0f2e61fd57b2ca454c3e6fcc583
Sourav Saha <shanto.sourav07@gmail.com>
2022-11-08 22:44:50 +0600

    Single Service Loading Done

--- src/Pages/Services/ServiceDetails.js
+++ src/Pages/Services/ServiceDetails.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
+    const { title, img, price, description } = useLoaderData();
+
     return (
-        <div>
-            <h1>Service Details</h1>
+        <div className='container mb-4'>
+            <h1 className='text-center'>Service Details</h1>
+
+            <Card>
+                <PhotoProvider>
+                    <PhotoView src={img}>
+                        <img src={img} alt="" />
+                    </PhotoView>
+                </PhotoProvider>
+
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text className='text-justify'>{description}</Card.Text>
+                    <Card.Text className='text-danger fw-bolder'>Price: ${price}</Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
